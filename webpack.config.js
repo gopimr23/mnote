e8a6252a5e5c254bf9121b9b2938f20ffa45d251
Radhakrishnan, Gopinath (ESI) <gopinath.radhakrishnan@adp.com>
2017-09-09 11:32:51 +0530

    God

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const srcPath = path.resolve(__dirname, "client/app");
+const destPath = path.resolve(__dirname, "client/dest");
+
+const config = {
+  entry: srcPath + "/index.js",
+  output: {
+    path: destPath,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: srcPath,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
+
+module.exports = config;
