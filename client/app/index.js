f271b277414655d4a1c11b831d522ab84030d697
Radhakrishnan, Gopinath (ESI) <gopinath.radhakrishnan@adp.com>
2017-09-10 07:47:25 +0530

    basic index html added.

--- client/app/index.js
+++ client/app/index.js
@@ -5,26 +5,54 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      test: ""
+      note: "",
+      isNotify : false,
+      notes: []
     }
   }
 
-  
+  saveNote (event){
+    console.log("SAVE NOTE --------- ", event.target);
+    this.setState({note: event.target.value});
+  }
+
+  saveNotify (event){
+    console.log("save notify * ", event.target);
+    this.setState({isNotify: event.target.value});
+  }
+
+  saveNewNote(event){
+    event.preventDefault();
+    let newNote = {
+      note: this.state.note,
+      isNotify: this.state.isNotify
+    };
+
+    this.state.notes.push(newNote);
+    console.log("New note", newNote);
+    this.setState({notes: this.state.notes});
+    this.setState({note : ""})
+    this.setState({isNotify : ""})
+  }
 
   render(){
     console.log("TEST RENDER ");
     return (
-      <form >
+      <form  onSubmit={this.saveNewNote.bind(this)}>
         <div>
           <label htmlFor="note">Note</label>
-          <textarea name="note" id="note" cols="30" rows="10" value={}></textarea>
+          <textarea name="note" id="note" cols="30" rows="10" value={this.state.note} onChange={this.saveNote.bind(this)}></textarea>
         </div>
         <div>
           <label htmlFor="">Enable notification</label>
-          <input type="radio" name="notification" value="yes" value={}/>
-          <input type="radio" name="notification" value="no" valu={}/>
+          <label htmlFor="yes">
+            <input type="radio" id="yes" name="notification" value="yes" checked={this.state.isNotify === "yes"} onChange={this.saveNotify.bind(this)}/>Yes
+          </label>
+          <label htmlFor="no">
+            <input type="radio" id="no" name="notification" value="no" checked={this.state.isNotify==="no"}  onChange={this.saveNotify.bind(this)}/>No
+          </label>
         </div>
-        <input type="submit" onsubmit={this.}/>
+        <input type="submit" value="Save"/>
       </form>
     );
   }
