746f3af31b6210b168a1a1d97a174d5fad71d711
Radhakrishnan, Gopinath (ESI) <gopinath.radhakrishnan@adp.com>
2017-09-13 08:21:33 +0530

    with couchbase test

--- server/controller/notes.js
+++ server/controller/notes.js
@@ -1,26 +1,12 @@
-const fs = require("fs");
-const {promisify} = require("util");
-const readFilePromise = promisify(fs.readFile);
-const path = require("path");
-
-let filePath = path.resolve(__dirname,"../notes.json");
+var db = require("../db/");
 
 var getNotes = ()=> {
-  return readFilePromise(filePath)
-    .then((data)=> {
-      console.log(data);
-      return JSON.parse(data);
-    });
+  return db.getNotes();
 };
 
-var saveNotes = (note, cb)=> {
-  return readFilePromise(filePath)
-    .then((data)=> {
-      var notes = JSON.parse(data);
-      notes.push(note);
-      notes = JSON.stringify(notes, null, 2);
-      fs.writeFile(filePath, notes, cb)
-    });
+var saveNotes = (note)=> {
+  console.log("in controller", note);
+  return db.saveNotes(note)
 };
 module.exports = {
   getNotes: getNotes,
--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,38 @@
-/**
- * Created by gradhak on 9/13/17.
- */
+const couchbase = require("couchbase");
+const Promise = require("bluebird");
+const uuid = require("uuid");
+let cluster = new couchbase.Cluster("couchbase://127.0.0.1");
+
+cluster.authenticate({username:"admin", password: "password2"});
+let notesBucket = cluster.openBucket("notes", "password2");
+
+function getNotes() {
+  var query = "select notes.* from notes";
+  console.log(query);
+  return new Promise(function (resolve, reject) {
+    console.log("in promise");
+    notesBucket.query(query, function (err, result) {
+      if (err) {
+        console.log("errrrrr ", err);
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+function saveNotes(newNote) {
+  console.log("in couchbase db ");
+  return new Promise(function (resolve, reject) {
+    var documentId = uuid.v4();
+    notesBucket.insert(documentId, newNote, function (err, data) {
+      if (err)console.log("ERRR ", err);
+      err ? reject(err) : resolve();
+    });
+  });
+}
+module.exports = {
+  getNotes: getNotes,
+  saveNotes: saveNotes
+};
