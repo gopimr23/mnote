217410ea7fa6c4c2798ba550b818df1fbadeefed
Radhakrishnan, Gopinath (ESI) <gopinath.radhakrishnan@adp.com>
2017-09-13 06:40:13 +0530

    server code added.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
+const notes = require("./notes");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static('dest'));
+
 
 app.get("/", (req, res)=> {
   res.status(200).sendFile(path.resolve(__dirname + "/../client/index.html"));
+});
+
+app.post("/note", (req, res)=> {
+  console.log(req.body);
+  notes.saveNotes(req.body, (err)=>{
+    if(err){
+      res.status(500).send(err);
+    }
+    else {
+      res.status(201).send("success");
+    }
+  });
+});
+
+app.get("/note", (req, res)=>{
+  notes.getNotes()
+    .then((data)=>{
+      console.log(data);
+      res.status(200).send(data);
+    });
 });
 
 app.listen(3000, ()=> {
---
+++ server/notes.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const {promisify} = require("util");
+const readFilePromise = promisify(fs.readFile);
+const path = require("path");
+
+let filePath = path.resolve(__dirname,"./notes.json");
+
+var getNotes = ()=> {
+  return readFilePromise(filePath)
+    .then((data)=> {
+      console.log(data);
+      return JSON.parse(data);
+    });
+};
+
+var saveNotes = (note, cb)=> {
+  return readFilePromise(filePath)
+    .then((data)=> {
+      var notes = JSON.parse(data);
+      notes.push(note);
+      notes = JSON.stringify(notes, null, 2);
+      fs.writeFile(filePath, notes, cb)
+    });
+};
+module.exports = {
+  getNotes: getNotes,
+  saveNotes: saveNotes
+};
